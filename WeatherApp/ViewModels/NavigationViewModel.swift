34b1eef73b5383c308caaeaafe7b4104e6a2a610
melissakobia <melissakobia1997@gmail.com>
2025-01-13 13:11:48 +0300

    Adds project structure

---
+++ WeatherApp/ViewModels/NavigationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationViewModel.swift
+//  WeatherApp
+//
+//  Created by Melissa Kobia on 13/01/2025.
+//
+
+import Foundation
+
+class NavigationViewModel: ObservableObject {
+    
+}
